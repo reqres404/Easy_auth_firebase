3ba3cb54e910f34e38eadf537051bcccebf916d7
Aditya Patil <adittyapatil89@gmail.com>
2022-12-23 16:38:19 +0530

    pushing the code

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,6 @@
 // const config = {
-//     apiKey: "AIzaSyD9aoqUFLQPXHgjBP5Qi07EwefwkspvSyo",
-//     authDomain: "easyauth-87504.firebaseapp.com",
-//     projectId: "easyauth-87504",
-//     storageBucket: "easyauth-87504.appspot.com",
-//     messagingSenderId: "776751875062",
-//     appId: "1:776751875062:web:3baa82e4f5045cf8283dd7"
-//   };
+//      add the fire base commit
+
+//  };
 
 export default config
